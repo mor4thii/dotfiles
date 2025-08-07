26fdac6752b4c9481d7ce5e96987bef7ccd0f4cb
Morathii <morathii@icloud.com>
2025-08-07 20:21:12 +0200

    :wrench: Make snacks explorer and picker show hidden files by default

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        hidden = true,
+        sources = {
+          files = {
+            hidden = true,
+          },
+        },
+      },
+      picker = {
+        hidden = true,
+        sources = {
+          files = {
+            hidden = true,
+          },
+        },
+      },
+    },
+  },
+}
