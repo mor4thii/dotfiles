9ba1220c86bc25ace98a52aa48b9520216429c3b
Morathii <morathii@icloud.com>
2025-12-21 15:57:51 +0100

    :wrench: Backup data w/ rsync but exclude more and more

--- backup/backup.sh
+++ backup/backup.sh
@@ -3,15 +3,18 @@ rsync -avh --delete --progress \
   --exclude='Games/battlenet' \
   --exclude='miniforge3' \
   --exclude='tmp' \
+  --exclude='.ansible' \
   --exclude='.cache' \
-  --exclude='.local/share/Steam' \
-  --exclude='.local/share/Trash' \
+  --exclude='.local' \
+  --exclude='.config' \
   --exclude='.cpan' \
   --exclude='.cpanm' \
   --exclude='.m2' \
+  --exclude='.mozilla' \
   --exclude='.npm' \
   --exclude='.sdkman' \
   --exclude='.thumbnails' \
+  --exclude='.var' \
   --exclude='.wine' \
   /home/fred/ /media/backup/fred-backup/
 echo "Backup complete!"
